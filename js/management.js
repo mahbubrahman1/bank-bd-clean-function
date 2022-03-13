abffcc8dbaf0b2ab4426a1bb29042bb5613aa795
mahbubrahman1 <mahbuburrofficial@gmail.com>
2022-03-13 09:41:09 +0600

    update deposit and withdraw

--- js/management.js
+++ js/management.js
@@ -1,2 +1,29 @@
+// amount from user
+function getInputAmount(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputInText = inputField.value;
+    const inputValue = parseFloat(inputInText);
+    inputField.value = '';
+    return inputValue;
+}
 
+// amount update in deposit field
+function updateTotal(totalFieldId, inputAmount) {
+    const totalField = document.getElementById(totalFieldId);
+    const previousTotalText = totalField.innerText;
+    const previousTotal = parseFloat(previousTotalText);
+    const newTotal = previousTotal + inputAmount;
+    totalField.innerText = newTotal;
+}
 
+// deposit..
+document.getElementById('deposit-button').addEventListener('click', function () {
+    const depositInputAmount = getInputAmount('deposit-input');
+    updateTotal('deposit-total', depositInputAmount);
+})
+
+// withdraw..
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const withdrawInputAmount = getInputAmount('withdraw-input');
+    updateTotal('withdraw-total', withdrawInputAmount);
+})
