3994fc54953c0632b575ae5b7ff5cfbaf7aeb4c3
mahbubrahman1 <mahbuburrofficial@gmail.com>
2022-03-13 10:15:13 +0600

    updated balance

--- js/management.js
+++ js/management.js
@@ -16,14 +16,30 @@ function updateTotal(totalFieldId, inputAmount) {
     totalField.innerText = newTotal;
 }
 
+// updating balance
+function updateBalance(inputAmount, isAdd) {
+    const balanceField = document.getElementById('balance-total');
+    const previousBalanceText = balanceField.innerText;
+    const previousBalance = parseFloat(previousBalanceText);
+    let newBalance;
+    if (isAdd == true) {
+        newBalance = previousBalance + inputAmount;
+    } else {
+        newBalance = previousBalance - inputAmount;
+    }
+    balanceField.innerText = newBalance;
+}
+
 // deposit..
 document.getElementById('deposit-button').addEventListener('click', function () {
     const depositInputAmount = getInputAmount('deposit-input');
     updateTotal('deposit-total', depositInputAmount);
+    updateBalance(depositInputAmount, true);
 })
 
 // withdraw..
 document.getElementById('withdraw-button').addEventListener('click', function () {
     const withdrawInputAmount = getInputAmount('withdraw-input');
     updateTotal('withdraw-total', withdrawInputAmount);
+    updateBalance(withdrawInputAmount, false);
 })
